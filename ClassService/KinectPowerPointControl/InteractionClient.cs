3ca2a5e3ecb4717787a0f54b592daf5b41068ea1
lucio cossio <lucio.cossio@gmail.com>
2013-04-28 17:38:01 -0300

    Added dll files and little interaction client.

---
+++ ClassService/KinectPowerPointControl/InteractionClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect.Toolkit.Interaction;
+
+namespace KinectPowerPointControl
+{
+    public class InteractionClient: IInteractionClient
+    {
+        public InteractionInfo GetInteractionInfoAtLocation(int skeletonTrackingId, InteractionHandType handType, double x, double y)
+        {
+            var interactionInfo = new InteractionInfo
+            {
+                IsPressTarget = true,
+                IsGripTarget = true
+            };
+
+            return interactionInfo;
+        }
+    }
+}
