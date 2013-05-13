2b3d78759b17b3e4d1b6bc8944f3ba4131064fb8
lucio cossio <lucio.cossio@gmail.com>
2013-05-12 22:57:05 -0300

    First Classes for global user skeleton state.

---
+++ ClassService/ServiceCore/GlobalSkeletonState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceCore
+{
+    public class GlobalSkeletonState
+    {
+        //private static UserSkeletonState userState;
+        public static UserSkeletonState UserState { get; set; }
+    }
+}
---
+++ ClassService/ServiceCore/UserSkeletonState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.Interaction;
+
+namespace ServiceCore
+{
+    public class UserSkeletonState
+    {
+        public Skeleton Skeleton { get; set; }
+        public UserInfo UserState { get; set; }        
+    }
+}
